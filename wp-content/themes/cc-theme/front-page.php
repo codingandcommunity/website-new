96e7b65384b8ed4fb610e0a7662f0e301e3c89f8
Joseph Lyon <joeylyon1@gmail.com>
2019-05-16 22:11:36 -0400

    add front page template start

---
+++ wp-content/themes/cc-theme/front-page.php
@@ -0,0 +1,96 @@
+<?php get_header(); ?>
+</div>
+<div class="hero is-medium">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <p class="title is-1 has-text-info has-text-weight-bold">Title text about C&&C</p>
+            <br>
+            <p class="subtitle">Sub title text about coding and community</p>
+        </div>
+        <div class="header-bg">
+            <div class="columns">
+                <div class="column">
+                    <div class="bar"></div>
+                </div>
+                <div class="column">
+                    <div class="bar"></div>
+                </div>
+                <div class="column">
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="bar"></div>
+                </div>
+                <div class="column">
+                    <div class="bar dark"></div>
+                </div>
+                <div class="column">
+                </div>
+                <div class="column">
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="bar"></div>
+                </div>
+                <div class="column">
+                </div>
+                <div class="column">
+                </div>
+
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <div class="bar dark"></div>
+                </div>
+                <div class="column">
+                    <div class="bar"></div>
+                </div>
+                <div class="column">
+                    <div class="bar dark"></div>
+                </div>
+                <div class="column">
+
+                </div>
+            </div>
+</div>
+    </div>
+</div>
+</div>
+</div>
+</section>
+<div class="hero is-medium has-background-info is-long">
+    <div class="hero-body">
+        <p class="title has-text-white">Code</p>
+    </div>
+</div>
+<div class="container hero is-large frontpage-mission-overlap">
+    <div class="columns is-centered">
+        <div class="column is-three-quarters">
+            <div class="box">
+                <div style="height: 500px;" class="columns">
+                    <div class="column bordered">
+                        <p class="title has-text-info">Content</p>
+                        <p class="subtitle has-text-info">Content</p>
+
+                    </div>
+                    <div class="column bordered">
+                        <p class="title has-text-info">Content</p>
+                        <p class="subtitle has-text-info">Content</p>
+
+                    </div>
+                    <div class="column bordered">
+                        <p class="title has-text-info">Content</p>
+                        <p class="subtitle has-text-info">Content</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>

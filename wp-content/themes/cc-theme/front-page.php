aa91f137595a9b8dcd65b9ed666afc726f839652
Joseph Lyon <joeylyon1@gmail.com>
2019-05-21 12:37:09 -0400

    minor structural cahnges

--- wp-content/themes/cc-theme/front-page.php
+++ wp-content/themes/cc-theme/front-page.php
@@ -18,63 +18,66 @@
     </div>
     </div>
 </section>
-<div class="hero">
-    <div class="hero-body">
-        <div class="container box">
-            <div class="columns">
-                <?php if (get_theme_mod('mission-img') != '#FFF') : ?>
+<section class="section">
+    <div class="hero">
+        <div class="hero-body">
+            <div class="container box">
+                <div class="columns">
+                    <?php if (get_theme_mod('mission-img') != '#FFF') : ?>
+                        <div class="column">
+                            <figure class="image ">
+                                <img src="<?php echo get_theme_mod('mission-img') ?>"></img>
+                            </figure>
+                        </div>
+                    <?php endif ?>
+                    <div class="column has-text-right">
+                        <p class="title is-1">Heading about mission statement</p>
+                        <p class="subtitle is-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. </p>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hero">
+        <div class="hero-body">
+            <div class="container is-fullheight">
+                <div class="columns">
                     <div class="column">
-                        <figure class="image ">
-                            <img src="<?php echo get_theme_mod('mission-img') ?>"></img>
-                        </figure>
+                        <div class="box">
+                            <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
+                            <p class="subtitle has-text-info">Content</p>
+                            <p class="">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
+                            </p>
+                        </div>
                     </div>
-                <?php endif ?>
-                <div class="column has-text-right">
-                    <p class="title is-1">Heading about mission statement</p>
-                    <p class="subtitle is-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. </p>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="hero">
-    <div class="hero-body">
-        <div class="container is-fullheight">
-            <div class="columns">
-                <div class="column">
-                    <div class="box">
-                        <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
-                        <p class="subtitle has-text-info">Content</p>
-                        <p class="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
-                        </p>
+                    <div class="column">
+                        <div class="box">
+                            <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
+                            <p class="subtitle has-text-info">Content</p>
+                            <p class="">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="column">
-                    <div class="box">
-                        <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
-                        <p class="subtitle has-text-info">Content</p>
-                        <p class="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
-                        </p>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="box">
-                        <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
-                        <p class="subtitle has-text-info">Content</p>
-                        <p class="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
-                        </p>
+                    <div class="column">
+                        <div class="box">
+                            <p class="title has-text-info"><i class="fas fa-laptop-code"></i> Content</p>
+                            <p class="subtitle has-text-info">Content</p>
+                            <p class="">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae libero vel ante imperdiet convallis in eu diam. Nam purus felis, euismod non accumsan ac, hendrerit at nisi. Nulla elit arcu, tincidunt nec arcu mattis, congue gravida ex. Nullam ornare lacus et enim laoreet, et lacinia ante bibendum. Morbi nec posuere mi. Aenean aliquet magna et auctor ornare. Aliquam congue ex at varius vestibulum. Maecenas eleifend dui vitae pulvinar bibendum. Duis faucibus elementum lobortis. Morbi feugiat, lectus id suscipit ullamcorper, purus purus consectetur nunc, sed cursus ex enim eget elit. Nunc porttitor nulla quam, sit amet tempor ligula convallis non. Nulla dictum sollicitudin leo, sollicitudin congue tortor fermentum at.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+            
         </div>
-        
+    
     </div>
 
-</div>
+</section>
 
 </body>
 

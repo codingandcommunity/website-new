7f7d2db67f1a3e840837cdf70ee0534c9b81e4bb
Joseph Lyon <joeylyon1@gmail.com>
2019-06-17 18:15:09 -0400

    push old stuff

--- wp-content/themes/cc-theme/front-page.php
+++ wp-content/themes/cc-theme/front-page.php
@@ -1,17 +1,16 @@
 <?php get_header(); ?>
-<section class="section">
+<section class="section" style="position: relative; overflow: hidden; padding-top: 0px;">
     <div style="background-image: url(<?php echo get_theme_mod('logo', 'none') ?>)" class="bg-image">
     </div>
     <div class="container">
-        <div class="hero is-medium">
+        <div class="hero is-large">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <p class="title is-massive has-text-info has-text-weight-black has-font-header"><?php echo get_theme_mod('title_text_block', 'TITLE TEXT') ?></p>
-                    <br>
-                    <p class="subtitle is-3 has-font-header"><?php echo get_theme_mod('subtitle_text_block', 'SUBTITLE TEXT') ?></p>
-                    <span class="button is-large is-info">Learn More</span>
-
-
+                    <div style="background-color: white; padding: 20px; border-radius: 2px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 0 0 1px rgba(0, 0, 0, 0.1); ">
+                        <p class="title is-1 has-text-info has-text-weight-black has-font-header"><?php echo get_theme_mod('title_text_block', 'TITLE TEXT') ?></p>
+                        <p class="subtitle is-5 has-font-header"><?php echo get_theme_mod('subtitle_text_block', 'SUBTITLE TEXT') ?></p>
+                        <span class="button is-info">Learn More</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,6 +19,44 @@
 </section>
 <section class="section"></section>
 <section class="section">
+    <div class="hero">
+        <div class="hero-title">
+            <div class="container has-text-centered">
+                <p class="title header-font">
+                    Our Partners
+                </p>
+            </div>
+        </div>
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-centered">
+                    <?php if (get_theme_mod('sponsor_one', '') != '') : ?>
+                        <div class="column is-3">
+                            <a href="<? get_theme_mod('sponsor_one_link', '#') ?>">
+                                <img src="<?php echo get_theme_mod('sponsor_one') ?>" />
+                            </a>
+                        </div>
+                    <?php endif ?>
+                    <?php if (get_theme_mod('sponsor_two', '') != '') : ?>
+                        <div class="column is-3">
+                            <a href="<? get_theme_mod('sponsor_two_link', '#') ?>">
+                                <img src="<?php echo get_theme_mod('sponsor_two') ?>" />
+                            </a>
+                        </div>
+                    <?php endif ?>
+                    <?php if (get_theme_mod('sponsor_three', '') != '') : ?>
+                        <div class="column is-3">
+                            <a href="<? get_theme_mod('sponsor_three_link', '#') ?>">
+                                <img src="<?php echo get_theme_mod('sponsor_three') ?>" />
+                            </a>
+                        </div>
+                    <?php endif ?>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
     <div class="hero">
         <div class="hero-body">
             <div class="container box">
@@ -33,7 +70,7 @@
                     <?php endif ?>
                     <div class="column has-text-right">
                         <p class="title is-1"><?php echo get_theme_mod('mission_text_block', 'Mission text') ?></p>
-                        <p class="subtitle is-4"><?php echo get_theme_mod('mission_body_block', 'Mission body')?></p>
+                        <p class="subtitle is-4"><?php echo get_theme_mod('mission_body_block', 'Mission body') ?></p>
                     </div>
                 </div>
             </div>
@@ -70,38 +107,7 @@
             </div>
         </div>
     </div>
-    <div class="hero">
-        <div class="hero-title">
-            <div class="container has-text-centered">
-                <p class="title header-font">
-                    Our Partners
-                </p>
-            </div>
-        </div>
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <?php if (get_theme_mod('sponsor_one', '') != '') : ?>
-                        <div class="column is-3">
-                            <img src="<?php echo get_theme_mod('sponsor_one') ?>" />
-                        </div>
-                    <?php endif ?>
-                    <?php if (get_theme_mod('sponsor_two', '') != '') : ?>
-                        <div class="column is-3">
-                            <img src="<?php echo get_theme_mod('sponsor_two') ?>" />
-                        </div>
-                    <?php endif ?>
-                    <?php if (get_theme_mod('sponsor_three', '') != '') : ?>
-                        <div class="column is-3">
-                            <img src="<?php echo get_theme_mod('sponsor_three') ?>" />
-                        </div>
-                    <?php endif ?>
-                </div>
-            </div>
-            
-        </div>
-    
-    </div>
+
 
 </section>
 
